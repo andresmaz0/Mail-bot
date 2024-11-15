dd4b5c10bcf2208754aa8186eb1330da49c6a89f
andresmaz0 <andresdavidmazo@gmail.com>
2024-11-15 12:59:47 -0600

    Creating a function send_email

--- App/main.py
+++ App/main.py
@@ -18,28 +18,32 @@ compose = """
 Congrats you a special discount in an Iphone 15 just follow the link below...
 """
 
-#Creating a copy of EmailMessage
-email = EmailMessage()
+def send_email(message, subject, reciever):
+    email_sender = "mailbotamazon@gmail.com"
+    email_sender_password = "mjwalajvxpooybre"
+    
+    #Creating a copy of EmailMessage
+    email = EmailMessage()
 
-#Setting who are going to send the message
-email["From"] = email_sender
+    #Setting who are going to send the message
+    email["From"] = email_sender
 
-#Setting who are going to recieve the message
-email["To"] = email_reciever
+    #Setting who are going to recieve the message
+    email["To"] = ','.join(reciever)
 
-email['Subject'] = subject
+    email['Subject'] = subject
 
-email.set_content(compose)
+    email.set_content(message)
 
-#this is useful to send in a secure way the email
-context = ssl.create_default_context()
+    #this is useful to send in a secure way the email
+    context = ssl.create_default_context()
 
-#that method allow to send a secured email through a server
-#1.- the server that will be useful to send the email
-#2.- the server port
-#3.- A secured context
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    #that method allow to send a secured email through a server
+    #1.- the server that will be useful to send the email
+    #2.- the server port
+    #3.- A secured context
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     #login in the sender account
-    smtp.login(email_sender, email_sender_password) 
+        smtp.login(email_sender, email_sender_password) 
 
-    smtp.sendmail(email_sender,email_reciever, email.as_string())
+        smtp.sendmail(email_sender,reciever, email.as_string())
